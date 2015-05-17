8a1c2413208bed6232617ef09c81e6f903b83ee3
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-05-17 23:07:02 +0200

    Verify running fixtures (currently only as string, not DOM)

--- test/specs/petSpec.js
+++ test/specs/petSpec.js
@@ -1,4 +1,8 @@
 describe("User interface", function() {
+    afterEach(function() {
+        fixtures.cleanUp();
+    });
+
     describe("settings menu", function() {
         // TODO: figure out, how to make before() resp. beforeEach() work
         // then put menu creation there
@@ -19,6 +23,16 @@ describe("User interface", function() {
             expect(menu.music).to.be.ok;
             menu.toggleSound();
             expect(menu.music).not.to.be.ok;
+        });
+
+        it("should load fixtures", function() {
+            fixtures.load("menu.html");
+            var body = fixtures.body();
+            /* FIXME: Figure out, why doc is null! 
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(body, "text/html");
+            */
+            expect(body).to.be.a("string");
         });
     });
 });
